9fdeac00277ca996f12518daa3388011a73b1f76
q-Mipe-p <arhe414@gmail.com>
2021-12-25 20:31:55 +0300

    Some changes

---
+++ StudentPersonalAccount/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using StudentPersonalAccount.MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentPersonalAccount.MVVM
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public ProfileViewModel ProfileVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            ProfileVM = new ProfileViewModel();
+        }
+    }
+}
