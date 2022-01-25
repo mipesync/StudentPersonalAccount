18c042ada17ebfd2bc3e1f76c3d433436d8abf48
q-Mipe-p <arhe414@gmail.com>
2022-01-25 22:07:53 +0300

    Some changes

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿var pass = "adpasdp1";
-var hashPass = BCrypt.Net.BCrypt.HashPassword(pass);
-
-var enterPass = "sdgdgsdgdg";
-var enterHashPass = BCrypt.Net.BCrypt.Verify(enterPass, hashPass);
-
-Console.WriteLine($"{pass} {hashPass}");
-
-Console.WriteLine($"{enterPass} {enterHashPass}");
